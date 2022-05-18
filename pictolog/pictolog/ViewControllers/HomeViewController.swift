0a7ad802857d8298db8f9599035df95c40fafd2e
Harmanpreet kaur <harmanpawar@HarmanpreetsAir.ht.home>
2022-05-17 23:12:57 -0400

    implemented segment controller  to switch btwn map view and collection view and addes collection view  delegate method  for cell size

--- pictolog/pictolog/ViewControllers/HomeViewController.swift
+++ pictolog/pictolog/ViewControllers/HomeViewController.swift
@@ -7,11 +7,14 @@
 
 import UIKit
 import CoreData
+import MapKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var segmentForView: UISegmentedControl!
     @IBOutlet weak var imgViewProfilePic: UIImageView!
     
+    @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var btnAddSite: UIButton!
     var sites: [NSManagedObject] = []
     
@@ -45,11 +48,26 @@ class HomeViewController: UIViewController {
         self.navigationController?.pushViewController(vc, animated: true)
     }
     
+    // function for segment
+    @IBAction func segmantBtnTapped(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            mapView.isHidden=false
+            collectionViewSites.isHidden=true
+        case 1 :
+            mapView.isHidden=true
+            collectionViewSites.isHidden=false
+        default:
+            print("do nothing for now")
+            
+        }
+    }
+    
 }
 
 // MARK: - CollectionView
 
-extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource , UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return sites.count
@@ -58,7 +76,12 @@ extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSour
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SiteCollectionViewCell", for: indexPath) as! SiteCollectionViewCell
         cell.lblSiteName.text! = sites[indexPath.row].value(forKeyPath: "name") as! String
+
         return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let screenSize=UIScreen.main.bounds
+        return CGSize(width: 170, height: 70)
     }
     
 }
